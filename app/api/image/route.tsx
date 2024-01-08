22a2967acdd1effa4f7ffd9d84ad2827b3b1de72
Anas ER-RAKIBI <anaserrakibi@gmail.com>
2024-01-08 13:35:51 +0100

    image generation

--- app/api/image/route.tsx
+++ app/api/image/route.tsx
@@ -13,7 +13,7 @@ export async function POST (req:Request){
     try{
             const {userId} = auth();
             const body = await req.json();
-            const {prompt, amount="1", resolution="512x512"} =body;
+            const {prompt, amount="1", resolution="512x512"} = body;
 
             if(!userId){
                 return new NextResponse("Unauthorized", {status:401});
